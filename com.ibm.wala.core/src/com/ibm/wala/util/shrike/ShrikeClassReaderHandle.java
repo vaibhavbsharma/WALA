b5b76f0b56c633d43501fdfe4ba1739d5447c8ef
sohah <husseinsoh@gmail.com>
2019-12-04 13:49:48 -0600

    adding in goto rewrite source code

--- com.ibm.wala.core/src/com/ibm/wala/util/shrike/ShrikeClassReaderHandle.java
+++ com.ibm.wala.core/src/com/ibm/wala/util/shrike/ShrikeClassReaderHandle.java
@@ -15,89 +15,112 @@ import com.ibm.wala.shrikeCT.ClassReader;
 import com.ibm.wala.shrikeCT.InvalidClassFileException;
 import com.ibm.wala.util.debug.Assertions;
 import com.ibm.wala.util.ref.CacheReference;
+import com.ibm.wala.util.shrike.gotoTransformation.GoToTransformer;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
 /**
  * A soft handle to a Shrike class reader
- *
+ * <p>
  * <p>TODO: implement more effective caching than just soft references TODO: push weakness up the
  * chain the InputStream, etc ... TODO: reduce reliance on reader throughout the analysis packages
  */
 public class ShrikeClassReaderHandle {
 
-  private static final boolean DEBUG = false;
-  /** The module entry that defines the class file */
-  private final ModuleEntry entry;
+    private static final boolean DEBUG = false;
+    /**
+     * The module entry that defines the class file
+     */
+    private final ModuleEntry entry;
 
-  private Object reader;
+    private Object reader;
 
-  /** The number of times we hydrate the reader */
-  private int hydrateCount = 0;
+    /**
+     * The number of times we hydrate the reader
+     */
+    private int hydrateCount = 0;
 
-  public ShrikeClassReaderHandle(ModuleEntry entry) {
-    if (entry == null) {
-      throw new IllegalArgumentException("null entry");
-    }
-    this.entry = entry;
-  }
-
-  /**
-   * @return an instance of the class reader ... create one if necessary
-   * @throws InvalidClassFileException iff Shrike fails to read the class file correctly.
-   */
-  public ClassReader get() throws InvalidClassFileException {
-    ClassReader result = (ClassReader) CacheReference.get(reader);
-    if (result == null) {
-      hydrateCount++;
-      if (DEBUG) {
-        if (hydrateCount > 1) {
-          System.err.println(("Hydrate " + entry + ' ' + hydrateCount));
-          try {
-            throw new Exception();
-          } catch (Exception e) {
-            e.printStackTrace();
-          }
+    public ShrikeClassReaderHandle(ModuleEntry entry) {
+        if (entry == null) {
+            throw new IllegalArgumentException("null entry");
         }
-      }
-      ByteArrayOutputStream S = new ByteArrayOutputStream();
-      try {
-        InputStream s = entry.getInputStream();
-        readBytes(s, S);
-        s.close();
-      } catch (IOException e) {
-        e.printStackTrace();
-        Assertions.UNREACHABLE();
-      }
-      result = new ClassReader(S.toByteArray());
-      reader = CacheReference.make(result);
+        this.entry = entry;
     }
-    return result;
-  }
 
-  /** Read is into bytes */
-  private static void readBytes(InputStream is, ByteArrayOutputStream bytes) throws IOException {
-    int n = 0;
-    byte[] buffer = new byte[1024];
-    while (n > -1) {
-      n = is.read(buffer, 0, 1024);
-      if (n > -1) {
-        bytes.write(buffer, 0, n);
-      }
+    /**
+     * @return an instance of the class reader ... create one if necessary
+     * @throws InvalidClassFileException iff Shrike fails to read the class file correctly.
+     */
+    public ClassReader get() throws InvalidClassFileException {
+        ClassReader result = (ClassReader) CacheReference.get(reader);
+        if (result == null) {
+            hydrateCount++;
+            if (DEBUG) {
+                if (hydrateCount > 1) {
+                    System.err.println(("Hydrate " + entry + ' ' + hydrateCount));
+                    try {
+                        throw new Exception();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            ByteArrayOutputStream S = new ByteArrayOutputStream();
+            try {
+                InputStream s = entry.getInputStream();
+                readBytes(s, S);
+
+                try {
+                    if (GoToTransformer.active) {
+                        Boolean wasRewritten = GoToTransformer.execute(entry.toString(), S.toByteArray());
+                        if (wasRewritten) {
+                            byte[] rewrittenClassBytes = GoToTransformer.reWrittenBytes;
+                            S = new ByteArrayOutputStream(rewrittenClassBytes.length);
+                            S.write(rewrittenClassBytes, 0, rewrittenClassBytes.length);
+                        }
+                    }
+                } catch (Exception e) {
+                    System.out.println("unable to rewrite for: " + this.entry.toString());
+                }
+                s.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+                Assertions.UNREACHABLE();
+            }
+            result = new ClassReader(S.toByteArray());
+            reader = CacheReference.make(result);
+        }
+        return result;
     }
-  }
 
-  public String getFileName() {
-    return entry.getName();
-  }
+    /**
+     * Read is into bytes
+     */
+    private static void readBytes(InputStream is, ByteArrayOutputStream bytes) throws IOException {
+        int n = 0;
+        byte[] buffer = new byte[1024];
+        while (n > -1) {
+            n = is.read(buffer, 0, 1024);
+            if (n > -1) {
+                bytes.write(buffer, 0, n);
+            }
+        }
+    }
 
-  /** Force the reference to be cleared/collected */
-  public void clear() {
-    reader = null;
-  }
+    public String getFileName() {
+        return entry.getName();
+    }
 
-  public ModuleEntry getModuleEntry() {
-    return entry;
-  }
+    /**
+     * Force the reference to be cleared/collected
+     */
+    public void clear() {
+        reader = null;
+    }
+
+    public ModuleEntry getModuleEntry() {
+        return entry;
+    }
 }
---
+++ com.ibm.wala.core/src/com/ibm/wala/util/shrike/gotoTransformation/VisitorPass.java
@@ -0,0 +1,5 @@
+package com.ibm.wala.util.shrike.gotoTransformation;
+
+public enum VisitorPass {
+    READINGPASS,WRITINGPASS;
+}
